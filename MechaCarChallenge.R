bc86e6719b1d1f5e18653775764375e9c92f459a
Chanise Smith <95304025+ChaniseS@users.noreply.github.com>
2022-04-03 18:04:16 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+#1. Linear Regression to Predict MPG
 #Use the library() function to load the dplyr package
 library(dplyr)
 
@@ -9,3 +10,23 @@ MechaCar_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground
 
 #Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(MechaCar_lm)
+
+# 2. Statistics on Suspension Coils
+#Import and read in the Suspension_Coil.csv file as a table.
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Create a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#3.T-Tests on Suspension Coils 
+#Write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(Suspension_Coil$PSI, mu=1500)
+
+#Write three more RScripts to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
+
